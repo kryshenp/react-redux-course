7c37062b746bf7fd44dd52a230b35d7e5a8f6e55
Pavlo Kryshenyk <pkryshenyk@pkryshenyk-cz-ltmp-x.local>
2020-07-06 12:35:02 +0200

    Handling Dynamic Content in the JavaScript Way

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -37,7 +37,6 @@ class App extends Component {
         { name: event.target.value, age: 27 },
         { name: "Jhonn", age: 66 },
       ],
-      showPersons: false,
     });
   };
 
@@ -50,6 +49,31 @@ class App extends Component {
       cursor: "pointer",
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Paooooooolo")}
+            changed={this.nameChangedHandler}
+          >
+            Hobbie: Raicing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -57,26 +81,7 @@ class App extends Component {
         <button onClick={() => this.togglePersonsHandler()} style={style}>
           Toggle persons
         </button>
-        {this.state.showPersons === true ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Paooooooolo")}
-              changed={this.nameChangedHandler}
-            >
-              Hobbie: Raicing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
     // return React.createElement(
