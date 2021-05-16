4c9566bf5a5e4a5c5a3160d228f2dda45ef096ec
Pavlo Kryshenyk <pkryshenyk@pkryshenyk-cz-ltmp-x.local>
2021-05-16 22:45:45 +0200

    pass data via props

--- course2021/may-application/src/components/ExpenseItem.js
+++ course2021/may-application/src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
-function ExpenseItem() {
-  return <h2>This is an Expense Item component</h2>;
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = "200.94";
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
